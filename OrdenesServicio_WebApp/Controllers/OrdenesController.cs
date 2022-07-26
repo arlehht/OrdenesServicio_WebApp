2e10b1b67e2dbd5c2a3db634d5b8edced9b6564b
arlehht <105451177+arlehht@users.noreply.github.com>
2022-07-26 17:50:01 -0500

    Correcciones

--- OrdenesServicio_WebApp/Controllers/OrdenesController.cs
+++ OrdenesServicio_WebApp/Controllers/OrdenesController.cs
@@ -10,6 +10,16 @@ namespace OrdenesServicio_WebApp.Controllers
     public class OrdenesController : Controller
     {
         OrdenesServicioEntities context = new OrdenesServicioEntities();
+
+        public ActionResult ListadoDeOrdenes() 
+        {
+            //Estamos mandando los datos para ser mostrados
+            List<OrdenesServicio> ordenes = context.OrdenesServicio.ToList();
+            ViewBag.data = ordenes;
+            
+            return View();
+        }
+
         // GET: Ordenes
         public ActionResult Ordenes()
         {
@@ -26,44 +36,45 @@ namespace OrdenesServicio_WebApp.Controllers
         public ActionResult Ordenes(OrdenesServicio ord)
         {
             string accion = Request.Form["boton"].ToString();
-            string eliminacion = Request.Form["eliminacion"].ToString();
-            string modificacion = Request.Form["modificacion"].ToString();
+            //string eliminacion = Request.Form["eliminacion"].ToString();
+            //string modificacion = Request.Form["modificacion"].ToString();
 
             switch (accion) 
             {
                 case "Guardar":
                     context.OrdenesServicio.Add(ord);
                     context.SaveChanges();
-                    TempData["msj"] = "Registro guardado";
+                    TempData["msj"] = "Guardado";
                     
                     break;
 
-                case "Modificar":
-                    if (modificacion.Equals("si"))
-                    {
-                        OrdenesServicio temp = context.OrdenesServicio.FirstOrDefault(x => x.Folio == ord.Folio);
-                        temp.Fecha = ord.Fecha;
-                        temp.Usuario = ord.Usuario;
-                        temp.Empresa = ord.Empresa;
-                        temp.TelCel = ord.TelCel;
-                        temp.Area = ord.Area;
-                        temp.Sucursal = ord.Sucursal;
-                        temp.Email = ord.Email;
-                        temp.Requerimiento = ord.Requerimiento;
-                        temp.Acciones = ord.Acciones;
-                        TempData["msj"] = "Registro moificado";
-                        context.SaveChanges();
-                    }
-                    break;
+                //case "Modificar":
+                //    //Editar
+                //    if (modificacion.Equals("si"))
+                //    {
+                //        OrdenesServicio temp = context.OrdenesServicio.FirstOrDefault(x => x.Folio == ord.Folio);
+                //        temp.Fecha = ord.Fecha;
+                //        temp.Usuario = ord.Usuario;
+                //        temp.Empresa = ord.Empresa;
+                //        temp.TelCel = ord.TelCel;
+                //        temp.Area = ord.Area;
+                //        temp.Sucursal = ord.Sucursal;
+                //        temp.Email = ord.Email;
+                //        temp.Requerimiento = ord.Requerimiento;
+                //        temp.Acciones = ord.Acciones;
+                //        TempData["msj"] = "Modificado";
+                //        context.SaveChanges();
+                //    }
+                //    break;
 
-                case "Eliminar":
-                    if (eliminacion.Equals("si"))
-                    {
-                        context.OrdenesServicio.Remove(context.OrdenesServicio.FirstOrDefault(x => x.Folio == ord.Folio));
-                        TempData["msj"] = "Registro eliminado";
-                        context.SaveChanges();
-                    }
-                    break;
+                //case "Eliminar":
+                //    if (eliminacion.Equals("si"))
+                //    {
+                //        context.OrdenesServicio.Remove(context.OrdenesServicio.FirstOrDefault(x => x.Folio == ord.Folio));
+                //        context.SaveChanges();
+                //        TempData["msj"] = "Eliminado";
+                //    }
+                //    break;
 
             }
             return RedirectToAction("Ordenes");
